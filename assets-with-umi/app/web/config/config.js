5d8f7979c51eb8c87659b864ee2b78938065f8c9
ycjcl868 <45808948@qq.com>
2020-03-02 07:18:56 +0800

    fix: publicPath

--- assets-with-umi/app/web/config/config.js
+++ assets-with-umi/app/web/config/config.js
@@ -1,6 +1,5 @@
 export default {
   presets: [ '@umijs/preset-react' ],
-  publicPath: 'http://localhost:8000/',
   antd: {},
   dynamicImport: {},
 };
--- assets-with-umi/app/web/config/config.local.js
+++ assets-with-umi/app/web/config/config.local.js
@@ -1,4 +1,5 @@
 export default {
+  publicPath: 'http://localhost:8000/',
   proxy: {
     '/restapi': {
       target: 'http://127.0.0.1:7001/',
